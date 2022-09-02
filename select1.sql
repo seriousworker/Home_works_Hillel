ed4a877bae839cb62d88b032f067d6ccc11724fd
Roman P <75758528+seriousworker@users.noreply.github.com>
2022-09-02 11:47:53 +0300

    Update select1.sql

--- select1.sql
+++ select1.sql
@@ -1,3 +1,5 @@
+-- Написать SELECT запрос для подсчёта общей суммы треков и их количества сгрупировав данные по TrackId.
+
 SELECT t.Name AS 'Name of the track', COUNT(ii.TrackId) AS 'Ordered times',  SUM(ii.UnitPrice) AS 'Total in cash'
 FROM invoice_items AS ii
 JOIN tracks AS t ON ii.TrackId = t.TrackId
