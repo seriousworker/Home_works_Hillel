6e0d95b157e3a93d0c3b3c7567f240bd1a1bb0d5
Roman P <75758528+seriousworker@users.noreply.github.com>
2022-09-02 11:53:17 +0300

    Update training_dicts.py

--- training_dicts.py
+++ training_dicts.py
@@ -1,3 +1,11 @@
+"""
+Дана строка (большАя строка, лучше взять на английском). Выведите слово, которое в этой строке встречается чаще всего. Если таких слов несколько, выведите последнее.
+
+Задачу необходимо решить с использованием словаря.
+
+"""
+
+
 given_string1 = 'Big black bug bit a big black dog on his big black nose'
 given_string2 = 'But good morning Good morning to ye and thou I’d say to all my patients because I was the worse of the hypocrites of all the hypocrites the cruel and phony hypocrites I was the very worst'
 given_string3 = 'Nory was a Catholic because her mother was a Catholic and Nory’s mother was a Catholic because her father was a Catholic and her father was a Catholic because his mother was a Catholic or had been'
