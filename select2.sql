d3797db862bad1b30067ad0541651a0ce33ae9fd
Roman P <75758528+seriousworker@users.noreply.github.com>
2022-09-02 11:49:14 +0300

    Update select2.sql

--- select2.sql
+++ select2.sql
@@ -1,3 +1,19 @@
+/* 
+
+Таблица: customers
+
+
+Вывести всех customers у которых FistName = 'Mark' и LastName = 'Taylor' или 
+
+FirstName = 'Frank' и LastName = 'Harris'
+
+
+В предложении WHERE для объединения нескольких условий можно использовать 
+
+операторы: AND и OR.
+
+*/
+
 SELECT CustomerId, FirstName, LastName, PhoneNumber, City, Country
 FROM customers
 WHERE (FirstName='Mark' AND LastName='Taylor') OR (FirstName='Frank' AND LastName='Harris')
